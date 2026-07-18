1ade876efe9315ce0dc73df6712a69d945dfc527
Shiva Bhusal <srollins175@gmail.com>
2026-07-18 22:14:33 +0545

    fix: update MongoDB connection URI to use environment variable

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,7 +4,7 @@ import { DatabaseController } from './database.controller';
 import { DatabaseService } from './database.service';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/Taskify')],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
   controllers: [DatabaseController],
   providers: [DatabaseService],
 })
