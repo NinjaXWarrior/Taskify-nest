f8fc9b266bf75ed83ee0c44513da36a03b997e56
Shiva Bhusal <srollins175@gmail.com>
2026-07-18 22:52:37 +0545

    fix: update MongooseModule initialization to include server selection timeout

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,8 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DatabaseController } from './database.controller';
 import { DatabaseService } from './database.service';
 
+console.log('MONGO_URI:', process.env.MONGO_URI);
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
+  // imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI!, {
+      serverSelectionTimeoutMS: 5000,
+    }),
+  ],
   controllers: [DatabaseController],
   providers: [DatabaseService],
 })
