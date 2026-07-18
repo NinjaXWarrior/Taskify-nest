27f737af845d94b5d8d1195422ea529896f6b0bb
Shiva Bhusal <srollins175@gmail.com>
2026-07-18 21:16:03 +0545

    chore: update .gitignore to include .env and remove default values from env.config

--- src/common/config/env.config.ts
+++ src/common/config/env.config.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const env = {
-  port: Number(process.env.PORT) || 3000,
-  mongoUri: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/taskify',
-  jwtSecret: process.env.JWT_SECRET || 'AshdbajAFCSC562u472t823757TFE76T7EU',
+  port: Number(process.env.PORT),
+  mongoUri: process.env.MONGO_URI,
+  jwtSecret: process.env.JWT_SECRET,
 };
